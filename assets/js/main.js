cb5a46b35608132e84eb85ebe92dfe7e3ab00e81
TheVedantPatil <162038413+TheVedantPatil@users.noreply.github.com>
2024-05-09 15:31:26 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,12 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
 sr.reveal('.home__social-icon', { interval: 200 });
 sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 }); 
+
+
+// Auto Typing Text
+var typed = new Typed(".auto-type", {
+    strings: ["EXTC Engineer.", "Table Tennis Player.", "and a Traveller."],
+    typeSpeed: 150,
+    backSpeed: 100,
+    looped: true
+})
